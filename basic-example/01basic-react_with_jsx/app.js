24ac6a528e5a70fa8be7721d8e39c2ae4b68bb45
Keren Mayorov <100772284+KerenMay@users.noreply.github.com>
2022-11-30 18:43:16 +0200

    added react and opening the "react-app" folder

--- basic-example/01basic-react_with_jsx/app.js
+++ basic-example/01basic-react_with_jsx/app.js
@@ -1,5 +1,10 @@
 function Hello() {
-  return <div>Hello to jsx</div>;
+  return (
+    <div>
+      <p>This is a paragraph</p>
+      <div>Hello to jsx</div>
+    </div>
+  );
 }
 const rootNode = document.getElementById("root");
 const root = ReactDOM.createRoot(rootNode);
