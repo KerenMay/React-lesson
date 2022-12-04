4179b86b369f29beff2f5366e0cd34c9dc89c276
Keren Mayorov <100772284+KerenMay@users.noreply.github.com>
2022-12-04 18:55:39 +0200

    add + and - buttons to counter

--- react-apps/src/applications/003_counter/counter.jsx
+++ react-apps/src/applications/003_counter/counter.jsx
@@ -5,15 +5,24 @@ function Counter() {
   //State
 
   const [counter, setCounter] = useState(0);
+  console.log("counter is rendering");
 
   function increment() {
-    setCounter(counter + 1);
-    console.log("increment", counter);
+    const newCounter = counter + 1;
+    setCounter(newCounter);
+    console.log("increment", newCounter);
+  }
+
+  function decrement() {
+    const decCounter = counter - 1;
+    setCounter(decCounter);
+    console.log("decrement", decCounter);
   }
 
   return (
     <div>
       <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
       <h2>{counter}</h2>
     </div>
   );
