b8c1413fddd737bd9f7ab68d29bd7de006b0b47e
Keren Mayorov <100772284+KerenMay@users.noreply.github.com>
2022-12-18 20:43:36 +0200

    grid in pageHeader (404)

--- react-apps/src/applications/real-app/503_AppAddPageHeader/pages/ErrorPage.jsx
+++ react-apps/src/applications/real-app/503_AppAddPageHeader/pages/ErrorPage.jsx
@@ -1,9 +1,29 @@
 import Layout from "../layouts/Layout";
+import PageHeader from "../components/PageHeader";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 function ErrorPage() {
   return (
     <Layout>
-      <h1>Error Stub</h1>
+      <PageHeader
+        title="OOOPS something went wrong"
+        subtitle={"not existed page"}
+      />
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={8} md={6} lg={6} xl={8}>
+          <Typography variant="h5">
+            Requested URL wasn't found in this server
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={4} md={6} lg={6} xl={4}>
+          <img
+            width="100%"
+            src="/assets/images/broken-robot.jpg"
+            alt="Broken Robot"
+          />
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
