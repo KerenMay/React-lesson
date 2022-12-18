ce6995d07e09087ece4b5200d3aab6193603934b
Keren Mayorov <100772284+KerenMay@users.noreply.github.com>
2022-12-18 19:48:59 +0200

    added 404 page route

---
+++ react-apps/src/applications/real-app/503_AppAddPageHeader/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import Layout from "../layouts/Layout";
+
+function ErrorPage() {
+  return (
+    <Layout>
+      <h1>Error Stub</h1>
+    </Layout>
+  );
+}
+
+export default ErrorPage;
--- react-apps/src/applications/real-app/503_AppAddPageHeader/router/Router.jsx
+++ react-apps/src/applications/real-app/503_AppAddPageHeader/router/Router.jsx
@@ -3,12 +3,14 @@ import AboutPage from "../pages/AboutPage";
 // import Layout from "./layouts/Layout";
 // import Button from "@mui/material/Button";
 import HomePage from "../pages/HomePage";
+import ErrorPage from "../pages/ErrorPage.jsx";
 import routes from "./routes";
 
 const router = createBrowserRouter([
   {
     path: routes.HOMEPAGE,
     element: <HomePage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: routes.ABOUT_PAGE,
